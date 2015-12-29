72f0eb0af8899fa809a5308373cdb5e624dd961b
koreain <korea100th@gmail.com>
2015-12-29 09:27:03 +0900

    Github for git pull

--- Hello.java
+++ Hello.java
@@ -6,5 +6,6 @@ public class Hello {
 		System.out.println("Hello Git!");
 		System.out.println("What is Git?");
 		System.out.println("That's DVCS");
+		system.out.println("That's version Control")
 }
 }
